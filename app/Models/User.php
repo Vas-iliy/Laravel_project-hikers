647b02bdc54452c57a71da2b2a287cbd678c6167
Vasiliy <kkolyasev1228@gmail.com>
2022-03-16 14:26:07 +0400

    login and registration model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,29 +11,19 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    public function role()
+    {
+        return $this->hasOne(Role::class);
+    }
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
